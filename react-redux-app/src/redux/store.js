0bddf9d950aaaaced3a1df472f1f9eb9a1678641
soucre.thaipvt <soucre.thaipvt@gmail.com>
2022-03-01 10:58:20 +0700

    Lab11: Redux Middleware| Xu ly trung tam trc khi dispacth

--- react-redux-app/src/redux/store.js
+++ react-redux-app/src/redux/store.js
@@ -1,9 +1,28 @@
-import {createStore, combineReducers} from 'redux';
-import todoReducer from './todo'
-
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import todoReducer from "./todo";
+// Action => Store => { Middleware => Reducer => State } => View => Action
 const reducer = combineReducers({
-    todo : todoReducer
+  todo: todoReducer,
 });
-const store = createStore(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-export default store;
+
+// function myMiddleware(store) {
+//   return function (next) {
+//       return function(action){
+
+//       }
+//   };
+// }
+const myMiddleware = store => next => action =>{
+    console.log('Action test', action, store.getState());
+    if(action.type === 'ADD_TODO' && action.payload === 'fuck'){
+        action.payload = '****'
+    }
+    return next(action); // truyen qua middleware tiep theo | middleware cuoi cung -> dispacth
+    // dung de xu ly data tu server ve: api
+}
+
+export default createStore(
+  reducer,
+  applyMiddleware(myMiddleware)
+  
+);
