8738eff6a69144e1e8ea8b18f139d0243626c334
soucre.thaipvt <soucre.thaipvt@gmail.com>
2022-03-01 17:36:49 +0700

    Lab17: npm i @reduxjs/toolkit || giai phap rut gon code toi uu || tich hop san thunk || middleware

--- react-redux-app/src/redux/store.js
+++ react-redux-app/src/redux/store.js
@@ -1,20 +1,19 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+// giai phap thay the store redux cu||tich hop san: thunk, middleware 
+// || code ngan gon hon
+import {configureStore} from '@reduxjs/toolkit'
+import { createLogger} from 'redux-logger';
 import todoReducer from "./todo";
 // giai phap thay the async fetch dispatch
 import thunk from 'redux-thunk';
 
-// Action => Store => { Middleware => Reducer => State } => View => Action
-const reducer = combineReducers({
-  todo: todoReducer,
-});
 
 // function myMiddleware(store) {
 //   return function (next) {
-//       return function(action){
-
-//       }
+//       return function(action){ }
 //   };
 // }
+
 const myMiddleware = store => next => action =>{ 
     console.log('Action test', action,'getState', store.getState());
     // action la 1 function
@@ -24,6 +23,7 @@ const myMiddleware = store => next => action =>{
     return next(action); // truyen qua middleware tiep theo | middleware cuoi cung -> dispacth
     // dung de xu ly data tu server ve: api
 }
+
 // const asyncMiddleware = store => next => action=>{
 //   console.log('Async',action) // la 1 function
 //   if(typeof action === 'function'){
@@ -32,7 +32,18 @@ const myMiddleware = store => next => action =>{
 //   return next(action)
 // }
 
-export default createStore(
-  reducer,
-  applyMiddleware(myMiddleware,thunk)
-);
+const logger = createLogger()
+
+// export default createStore(
+//   reducer,
+//   applyMiddleware(logger, thunk, myMiddleware)
+// );
+
+// Action => Store => { Middleware => Reducer => State } => View => Action
+const reducer = combineReducers({
+  todo: todoReducer,
+});
+
+export default configureStore({
+  reducer
+})
